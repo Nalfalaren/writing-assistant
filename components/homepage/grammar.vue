4695ee252375c39356115da7c6cc54ba99d75b56
Nalfalaren <hieuchuu138@gmail.com>
2024-06-18 12:53:07 +0700

    fix ui

--- components/homepage/grammar.vue
+++ components/homepage/grammar.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="relative flex flex-col justify-center items-center">
+  <div class="relative flex flex-col justify-center">
     <homepage-small_nav v-if="$router.currentRoute.value.fullPath !== '/'"/>
     <div>
     <UContainer>
-      <div class="absolute top-[25%] left-[20%]">
+      <div class="absolute top-[25%] left-[20%] max-w-[1400px] h-screen">
         <div class="px-32">
           <div class="relative aspect-video w-full max-h-[800px]">
             <form
