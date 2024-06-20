d7d56e513298809e8bf29a34b22372981ced21ed
Nalfalaren <hieuchuu138@gmail.com>
2024-06-21 00:49:42 +0700

    fix

--- components/homepage/text_completion.vue
+++ components/homepage/text_completion.vue
@@ -93,7 +93,7 @@
 <script setup lang="js">
 import { ref } from 'vue';
 import SuccessMessage from '../alert/SuccessAlert';
-import getTextCompletion from '~/composables/getTextCompletion'
+import getTextCompletion from '~/composables/getTextCompletion';
 
 let rawText = ref('');
 let paraphrasedAnswer = ref('');
